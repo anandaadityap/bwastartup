3367a36e5836fd92c42a5f995e2f8776da393864
Ananda Aditya Putra <anandaadityaputra11@gmail.com>
2024-07-29 08:17:36 +0700

    Create endpoint email availability

--- handler/user.go
+++ handler/user.go
@@ -70,3 +70,38 @@ func (h *userHandler) Login(c *gin.Context) {
 	response := helper.ApiResponse("Login Successfully", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+	// input email
+	var input user.CheckEmailInput
+	// input email di mapping
+	if err := c.ShouldBindJSON(&input); err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.ApiResponse("Email checker failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+	// struct input di pasing ke service
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server error"}
+		response := helper.ApiResponse("Email unavailable", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{
+		"is_available": isEmailAvailable,
+	}
+
+	metaMessage := "Email has been registered"
+	metaStatus := "failed"
+	if isEmailAvailable {
+		metaMessage = "Email available"
+		metaStatus = "success"
+	}
+	// service manggil repository apakah email sudah ada atau tidak
+	response := helper.ApiResponse(metaMessage, http.StatusOK, metaStatus, data)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -25,5 +25,6 @@ func main() {
 	api := r.Group("/api/v1")
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	r.Run()
 }
