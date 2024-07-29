c4c441885efa32c50562df0a9544877c63a94905
Ananda Aditya Putra <anandaadityaputra11@gmail.com>
2024-07-29 10:29:02 +0700

    Create validation token JWT

--- auth/service.go
+++ auth/service.go
@@ -1,13 +1,17 @@
 package auth
 
 import (
+	"errors"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
+var JWT_SECRET_KEY = []byte(os.Getenv("JWT_SECRET_KEY"))
+
 type Service interface {
 	GenerateToken(userId int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -23,11 +27,24 @@ func (s *jwtService) GenerateToken(userId int) (string, error) {
 	claims["user_id"] = userId
 
 	// claim dan sign jwt
-	JWT_SECRET_KEY := []byte(os.Getenv("JWT_SECRET_KEY"))
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err := token.SignedString(JWT_SECRET_KEY)
 	if err != nil {
 		return signedToken, err
 	}
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("invalid token")
+		}
+		return []byte(JWT_SECRET_KEY), nil
+	})
+	if err != nil {
+		return token, err
+	}
+	return token, nil
 }
